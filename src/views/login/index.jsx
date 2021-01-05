9bbc688334e75fb14617f802ce7db5addf7aa135
unknown <634206380@qq.com>
2021-01-05 14:24:46 +0800

    游戏提现页面和营销登录页统一入口地址

--- src/views/login/index.jsx
+++ src/views/login/index.jsx
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { Button, Flex, Toast, WhiteSpace } from "antd-mobile";
+import {
+  Button,
+  Flex,
+  Toast,
+  WhiteSpace,
+  ActivityIndicator,
+} from "antd-mobile";
 import { Redirect } from "react-router-dom";
 import iconUrl from "../../assets/icon.png";
 import loginUrl from "../../assets/login.png";
@@ -7,116 +13,158 @@ import "./index.css";
 import request from "../../utils/request";
 
 export default function Login(props) {
-    let { params } = props.match;
-    let base64UserInfo = params.userInfo ? decodeURIComponent(params.userInfo) : "";
-    const [loading, setLoading] = useState(false);
-    const [isLogin, setIsLogin] = useState(false);
-    const [userInfo, setUserInfo] = useState({});
-    useEffect(() => {
-        (function (d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) { return; }
-            js = d.createElement(s); js.id = id;
-            js.src = "https://connect.facebook.net/en_US/sdk.js";
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
+  let { params } = props.match;
+  let base64UserInfo = params.userInfo
+    ? decodeURIComponent(params.userInfo)
+    : "";
+  const [loading, setLoading] = useState(false);
+  const [firstIn, setFirstIn] = useState(true);
+  const [isRedeem, setIsRedeem] = useState(false);
+  const [data, setData] = useState({ assets: [] });
+  const [isLogin, setIsLogin] = useState(false);
+  const [userInfo, setUserInfo] = useState({});
+  useEffect(() => {
+    (function (d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {
+        return;
+      }
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, "script", "facebook-jssdk");
 
-        window.fbAsyncInit = () => {
-            window.FB?.init({
-                appId: "1026987934476519",
-                cookie: true,
-                xfbml: true,
-                version: "v9.0",
-            });
-        };
-
-        window.analytics.logEvent("enter_login_page");
-    }, []);
-
-    const queryData = async () => {
-        setLoading(true);
-        if (!base64UserInfo) {
-            Toast.fail("invalid params!");
-            return;
-        }
-
-        let { data } = await request.post("/marketing/infoVerify", base64UserInfo);
-
-        if (data?.facebookBound) {
-            setIsLogin(true);
-        }
-        setUserInfo({
-            accountId: data.accountId,
-            platform: data.platform
-        });
-        setLoading(false);
+    window.fbAsyncInit = () => {
+      window.FB?.init({
+        appId: "1026987934476519",
+        cookie: true,
+        xfbml: true,
+        version: "v9.0",
+      });
     };
 
-    useEffect(() => {
-        if (params.userInfo) {
-            queryData();
-        }
-    }, []);
+    window.analytics.logEvent("enter_login_page");
+  }, []);
 
+  const queryData = async () => {
+    if (!base64UserInfo) {
+      Toast.fail("invalid params!");
+      return;
+    }
 
+    let { data } = await request.post("/marketing/infoVerify", base64UserInfo);
+    data.redeemType = 1;
+    if (data?.redeemType == 1) {
+      setIsRedeem(true);
+      setData(data);
+    } else {
+      if (data?.facebookBound) {
+        setIsLogin(true);
+      }
 
-    function handleFbLogin() {
-        setLoading(true);
-        window.analytics.logEvent("click_fb_login");
-        window.FB?.login(function ({ authResponse, status }) {
-            if (status === 'connected') {
-                let params = {
-                    userEmail: "",
-                    accountId: userInfo.accountId,
-                    userId: authResponse.userID,
-                    type: "Facebook",
-                    appId: "1026987934476519",
-                    token: authResponse.accessToken,
-                    userName: "",
-                    platform: userInfo.platform,
-                    boundDevice: userInfo.platform,
-                    originalMessage: JSON.stringify(authResponse)
-                }
-                window.FB.api('/me?fields=email,id,name&access_token=' + authResponse.accessToken, async (response) => {
-                    params.userName = response.name;
-                    params.userEmail = response.email;
-                    let { data } = await request.post("/thirdPartyUser/bind", params);
-                    if (data.accountId) {
-                        window.analytics.logEvent("click_fb_login_success");
-                        setIsLogin(true);
-                    }
-                });
-            } else {
-                window.analytics.logEvent("click_fb_login_failed");
-                Toast.Fail('facebook login failed!');
+      setUserInfo({
+        accountId: data.accountId,
+        platform: data.platform,
+      });
+    }
+    setFirstIn(false);
+  };
+
+  useEffect(() => {
+    if (params.userInfo && firstIn) {
+      queryData();
+    }
+  }, []);
+
+  function handleFbLogin() {
+    setLoading(true);
+    window.analytics.logEvent("click_fb_login");
+    window.FB?.login(
+      function ({ authResponse, status }) {
+        if (status === "connected") {
+          let params = {
+            userEmail: "",
+            accountId: userInfo.accountId,
+            userId: authResponse.userID,
+            type: "Facebook",
+            appId: "1026987934476519",
+            token: authResponse.accessToken,
+            userName: "",
+            platform: userInfo.platform,
+            boundDevice: userInfo.platform,
+            originalMessage: JSON.stringify(authResponse),
+          };
+          window.FB.api(
+            "/me?fields=email,id,name&access_token=" + authResponse.accessToken,
+            async (response) => {
+              params.userName = response.name;
+              params.userEmail = response.email;
+              let { data } = await request.post("/thirdPartyUser/bind", params);
+              if (data.accountId) {
+                window.analytics.logEvent("click_fb_login_success");
+                setIsLogin(true);
+              }
             }
-            setLoading(false);
-        }, { scope: 'public_profile,email' });
-    }
+          );
+        } else {
+          window.analytics.logEvent("click_fb_login_failed");
+          Toast.Fail("facebook login failed!");
+        }
+        setLoading(false);
+      },
+      { scope: "public_profile,email" }
+    );
+  }
 
-    //已经绑定了facebook就跳过登录
-    if (isLogin) {
-        return <Redirect to={`/home/${base64UserInfo}`} />
-    }
-
+  if (firstIn && base64UserInfo !== "") {
     return (
-        <Flex align="center" justify="center" className="login" direction="column">
-            <img alt="" height="120" src={iconUrl} />
-            <WhiteSpace size="xl"></WhiteSpace>
-            <h2>INVITE AND</h2>
-            <h1>RECEIVE</h1>
-            <h1 className="money">CASH PRIZE</h1>
-            <p>Invite 3 friends for <span style={{ color: "#116aad" }}>$1</span>.<br />10 friends for <span style={{ color: "#116aad" }}>$4</span>.</p>
-            <Button
-                style={{
-                    width: "70vw",
-                    backgroundImage: `url(${loginUrl})`,
-                    backgroundSize: "100% 100%"
-                }}
-                onClick={handleFbLogin}
-                disabled={loading}
-                loading={loading}>
-            </Button>
-        </Flex>
-    )
+      <div
+        style={{
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <ActivityIndicator size="large" text="loading..."></ActivityIndicator>
+      </div>
+    );
+  }
+
+  //跳转体现页面
+  if (isRedeem) {
+    return <Redirect to={`/redeem/${btoa(JSON.stringify(data))}`} />;
+  }
+
+  //已经绑定了facebook就跳过登录
+  if (isLogin) {
+    return <Redirect to={`/home/${base64UserInfo}`} />;
+  }
+
+  return (
+    <Flex align="center" justify="center" className="login" direction="column">
+      <img alt="" height="120" src={iconUrl} />
+      <WhiteSpace size="xl"></WhiteSpace>
+      <h2>INVITE AND</h2>
+      <h1>RECEIVE</h1>
+      <h1 className="money">CASH PRIZE</h1>
+      <p>
+        Invite 3 friends for <span style={{ color: "#116aad" }}>$1</span>.<br />
+        10 friends for <span style={{ color: "#116aad" }}>$4</span>.
+      </p>
+      <Button
+        style={{
+          width: "70vw",
+          backgroundImage: `url(${loginUrl})`,
+          backgroundSize: "100% 100%",
+        }}
+        onClick={handleFbLogin}
+        disabled={loading}
+        loading={loading}
+      ></Button>
+    </Flex>
+  );
 }
--- src/views/redeem/index.jsx
+++ src/views/redeem/index.jsx
@@ -1,29 +1,12 @@
-import React, { useEffect, useState } from "react";
-import { Modal } from "antd-mobile";
+import React from "react";
+import { Modal, Result } from "antd-mobile";
 import "./index.css";
-// import rightBtn from "../../assets/records.png";
-import request from "../../utils/request";
 import config from "../../config/config";
 
 export default function Redeem({ match }) {
   const { userInfo } = match.params;
-  const [assets, setAssets] = useState([]);
-  const [gameName, setGameName] = useState("");
-  const queryData = async () => {
-    if (!userInfo) {
-      return;
-    }
-
-    let { data } = await request.post("/marketing/infoVerify", userInfo);
-    setAssets(data?.assets || []);
-    setGameName(data.appName);
-  };
-
-  useEffect(() => {
-    if (userInfo) {
-      queryData();
-    }
-  }, []);
+  let gameInfo = null;
+  let assetNods = [];
 
   const checked = () => {
     Modal.alert(
@@ -33,42 +16,48 @@ export default function Redeem({ match }) {
     );
   };
 
-  let gameInfo = null;
-  for (let i = 0; i < config.hotGames.length; ++i) {
-    let game = config.hotGames[i];
-    if (gameName.toLowerCase() === game.name.toLowerCase()) {
-      gameInfo = game;
-      break;
+  try {
+    let user = JSON.parse(atob(userInfo));
+    let { appName: gameName, assets } = user;
+    for (let i = 0; i < config.hotGames.length; ++i) {
+      let game = config.hotGames[i];
+      if (gameName.toLowerCase() === game.name.toLowerCase()) {
+        gameInfo = game;
+        break;
+      }
     }
-  }
-  let assetNods = [];
-  if (gameName && gameInfo) {
-    for (let i = 0; i < assets.length; ++i) {
-      let code = Number(assets[i].code);
-      assetNods.push(
-        <div key={code} className="jss3">
-          <div className="jss5 item-wbg">
-            <img
-              alt="icon"
-              src={gameInfo.assets[code].default}
-              className="jss4"
-            />
-            <div className="jss7">
-              <p className="jss8">
-                {assets[i].targetValue} = ${assets[i].award}
-              </p>
-              <div className="jss9">
-                <span className="user_coins">{assets[i].value}</span> /{" "}
-                {assets[i].targetValue}
+
+    if (gameName && gameInfo) {
+      for (let i = 0; i < assets.length; ++i) {
+        let code = Number(assets[i].code);
+        assetNods.push(
+          <div key={code} className="jss3">
+            <div className="jss5 item-wbg">
+              <img
+                alt="icon"
+                src={gameInfo.assets[code]?.default}
+                className="jss4"
+              />
+              <div className="jss7">
+                <p className="jss8">
+                  {assets[i].targetValue} = ${assets[i].award}
+                </p>
+                <div className="jss9">
+                  <span className="user_coins">{assets[i].value}</span> /{" "}
+                  {assets[i].targetValue}
+                </div>
               </div>
             </div>
+            <div className="jss6 item-paypal-bg" onClick={checked}>
+              <p className="jss10">${assets[i].award}</p>
+            </div>
           </div>
-          <div className="jss6 item-paypal-bg" onClick={checked}>
-            <p className="jss10">${assets[i].award}</p>
-          </div>
-        </div>
-      );
+        );
+      }
     }
+  } catch (err) {
+    console.log(err);
+    return <Result title="something went wrong!"></Result>;
   }
 
   return (
